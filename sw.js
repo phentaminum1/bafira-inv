2bc852ddb9843cdec14e740c818e12fc25f6193f
phentaminum1 <rama.ar4u@gmail.com>
2025-12-31 06:28:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,8 @@
+// sw.js DINONAKTIFKAN SEMENTARA
 self.addEventListener("install", () => {
-  console.log("Service Worker installed");
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", () => {
-  console.log("Service Worker activated");
-});
-
-self.addEventListener("push", event => {
-  const data = event.data.json();
-
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: "icon-192.png",
-    badge: "icon-192.png"
-  });
+  self.clients.claim();
 });
